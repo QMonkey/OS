b5dba778aacf7770eb9731f4048482e01945caae
QMKQMonkey <qmkqmonkey@gmail.com>
2014-05-29 22:35:30 +0800

    Fix the constant bug

--- test/protect_mode.asm
+++ test/protect_mode.asm
@@ -4,37 +4,36 @@
 	org	07c00h
 	jmp	boot
 
-; GDT
 GDT:
 	PMDescriptor 0, 0, 0
 GDT_CODE32:
 	PMDescriptor 0, Code32Len - 1, DA_CE + DA_CD32
+GDT_CODE16:
+	PMDescriptor 0, 0ffffh, DA_CE
+GDT_DATA:
+	PMDescriptor 0, DataLen - 1, DA_DRW
 GDT_VIDEO:
 	PMDescriptor 0b8000h, 0ffffh, DA_DRW
-; GDT End
 
-GdtLen	equ	$ - GDT
+GdtLen	equ	$-GDT
 GdtPtr	dw	GdtLen - 1
 	dd	0
 
 SelectorCode32	equ	GDT_CODE32 - GDT
+SelectorCode16	equ	GDT_CODE16 - GDT
+SelectorData	equ	GDT_DATA - GDT
 SelectorVideo	equ	GDT_VIDEO - GDT
+
+data_segment:
+	resb	26
+
+DataLen	equ	$-data_segment
 
 boot:
 	mov	ax, cs
 	mov	ds, ax
 	mov	es, ax
 	mov	ss, ax
-	mov	sp, 0100h
-
-;	mov	ax, rmStr
-;	mov	bp, ax
-;	mov	ah, 13h
-;	mov	al, 01h
-;	mov	cx, rmLen
-;	mov	dx, 0
-;	mov	bx, 000ch
-;	int	10H
 
 	xor	eax, eax
 	mov	ax, cs
@@ -44,6 +43,24 @@ boot:
 	shr	eax, 16
 	mov	[GDT_CODE32 + 4], al
 	mov	[GDT_CODE32 + 7], ah
+
+	xor	eax, eax
+	mov	ax, cs
+	shl	eax, 4
+	add	eax, Code16
+	mov	[GDT_CODE16 + 2], ax
+	shr	eax, 16
+	mov	[GDT_CODE16 + 4], al
+	mov	[GDT_CODE16 + 7], ah
+
+	xor	eax, eax
+	mov	ax, ds
+	shl	eax, 4
+	add	eax, data_segment
+	mov	[GDT_DATA + 2], ax
+	shr	eax, 16
+	mov	[GDT_DATA + 4], al
+	mov	[GDT_DATA + 7], ah
 
 	xor	eax, eax
 	mov	ax, ds
@@ -65,26 +82,7 @@ boot:
 	jmp	dword	SelectorCode32: 0
 
 Code32:
-	mov	ax, SelectorVideo
-	mov	gs, ax
+	mov	ax, SelectorData
+	mov	ds, ax
 	xor	edi, edi
-	mov	edi, (80 * 11 + 79) * 2
-	mov	ah, 0ch
-	mov	al, 'P'
-	mov	[gs:edi], ax
-
-halt:
-	hlt
-	jmp	halt
-
-Code32Len	equ	$-Code32
-
-rmStr:
-	db	"Real Mode", 0ah, 0dh
-rmLen	equ	$-rmStr
-pmStr:
-	db	"Protect Mode", 0ah, 0dh
-pmLen	equ	$-pmStr
-
-	resb	510-($-$$)
-	dw	0xaa55
+	cld
