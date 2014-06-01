404630fffff8253934db8aea91784248b7ef2063
QMKQMonkey <QMKQMonkey@gmail.com>
2014-06-01 13:14:21 +0800

    Fix the boot bug.

--- boot/boot.asm
+++ boot/boot.asm
@@ -4,15 +4,14 @@ org	07c00h
 	jmp	boot
 
 boot:
-	mov	ax, cs
-	mov	ds, ax
-	mov	es, ax
+	mov ax, cs
+	mov ds, ax
+	mov es, ax
 	mov	fs, ax
 	mov	gs, ax
-	mov	ss, ax
-	mov	sp, 100h
+	mov ss, ax
+	mov sp, 07b00h
 
-.clear:
 	; Clear screen
 	mov	ah, 06h
 	xor	al, al
@@ -22,29 +21,28 @@ boot:
 	mov	dx, 184fh
 	int	10h
 
-	; Display .booting
+	; Display booting
 	mov	ax, cs
 	mov	es, ax
-	mov	ax, .booting
+	mov	ax, booting
 	push	ax
 	xor	ax, ax
 	push	ax
-	mov	ax, .booting_len
+	mov	ax, booting_len
 	push	ax
-	call	.display_str
+	call	display_str
 	add	sp, 6
 
-.load:
-	; Display .loading
+	; Display loading
 	mov	ax, cs
 	mov	es, ax
-	mov	ax, .loading
+	mov	ax, loading
 	push	ax
 	mov	ax, 0100h
 	push	ax
-	mov	ax, .loading_len
+	mov	ax, loading_len
 	push	ax
-	call	.display_str
+	call	display_str
 	add	sp, 6
 
 	; Reset floppy
@@ -52,7 +50,6 @@ boot:
 	xor	dl, dl
 	int	13h
 
-	; Read loader
 	mov	ax, LD_BASE
 	mov	es, ax
 	mov	bx, LD_OFFSET
@@ -62,32 +59,32 @@ boot:
 	mov	cl, 2
 	xor dx, dx
 	int	13h
-	jnc	.load_err
-	test	ah, ah
-	jnz	.load_err
 
-	; Display .load_successfully
+	test	ah, ah
+	jnz	load_err
+
+	; Display load_successfully
 	mov	ax, cs
 	mov	es, ax
-	mov	ax, .load_successfully
+	mov	ax, load_successfully
 	push	ax
 	mov	ax, 0200h
 	push	ax
-	mov	ax, .load_successfully_len
+	mov	ax, load_successfully_len
 	push	ax
-	call	.display_str
+	call	display_str
 	add	sp, 6
 
-	; Display .checking
+	; Display checking
 	mov	ax, cs
 	mov	es, ax
-	mov	ax, .checking
+	mov	ax, checking
 	push	ax
 	mov	ax, 0300h
 	push	ax
-	mov	ax, .checking_len
+	mov	ax, checking_len
 	push	ax
-	call	.display_str
+	call	display_str
 	add	sp, 6
 
 	; Check loader
@@ -96,11 +93,11 @@ boot:
 	mov	bx, LD_OFFSET
 	mov	ax, [es: bx + LD_SIZE - 2]
 	cmp	ax, 0aa55h
-	jne	.invalid_loader_err
+	jne	invalid_loader_err
 
 	jmp	LD_BASE: LD_OFFSET
 
-.display_str:
+display_str:
 	push	bp
 	mov	bp, sp
 	push	ax
@@ -112,7 +109,7 @@ boot:
 	mov	dx, [bp + 6]
 	mov	ax, [bp + 8]
 	mov	bp, ax
-	mov	ax, 1301h
+	mov	ax, 1300h
 	mov	bx, 0007h
 	int	10h
 
@@ -123,59 +120,59 @@ boot:
 	pop	bp
 	ret
 
-.load_err:
-	; Display .load_err
+load_err:
+	; Display load_err and halt
 	mov	ax, cs
 	mov	es, ax
-	mov	ax, .load_err_msg
+	mov	ax, load_err_msg
 	push	ax
 	mov	ax, 0200h
 	push	ax
-	mov	ax, .load_err_msg_len
+	mov	ax, load_err_msg_len
 	push	ax
-	call	.display_str
-	jmp	.halt
+	call	display_str
+	jmp	halt
 
-.invalid_loader_err:
-	; Display .invalid_loader_err
+invalid_loader_err:
+	; Display invalid_loader_err and halt
 	mov	ax, cs
 	mov	es, ax
-	mov	ax, .invalid_loader_err_msg
+	mov	ax, invalid_loader_err_msg
 	push	ax
 	mov	ax, 0400h
 	push	ax
-	mov	ax, .invalid_loader_err_msg_len
+	mov	ax, invalid_loader_err_msg_len
 	push	ax
-	call	.display_str
-	jmp	.halt
+	call	display_str
+	jmp	halt
 
-.halt:
+halt:
 	hlt
-	jmp	.halt
-
-.booting:
+	jmp	halt
+	
+booting:
 	db	"Booting..."
-.booting_len	equ	$ - .booting
+booting_len	equ	$ - booting
 
-.loading:
+loading:
 	db	"Loading..."
-.loading_len	equ	$ - .loading
+loading_len	equ	$ - loading
 
-.load_err_msg:
+load_err_msg:
 	db	"Failed to load!"
-.load_err_msg_len	equ	$ - .load_err_msg
+load_err_msg_len	equ	$ - load_err_msg
 
-.load_successfully:
+load_successfully:
 	db	"Successfully load the loader!"
-.load_successfully_len	equ	$ - .load_successfully
+load_successfully_len	equ	$ - load_successfully
 
-.checking:
+checking:
 	db	"Checking..."
-.checking_len	equ	$ - .checking
+checking_len	equ	$ - checking
 
-.invalid_loader_err_msg:
+invalid_loader_err_msg:
 	db	"Invalid loader!"
-.invalid_loader_err_msg_len	equ	$ - .invalid_loader_err_msg
+invalid_loader_err_msg_len	equ	$ - invalid_loader_err_msg
 
 	resb	510 - ($ - $$)
 	db	55h, 0aah
--- boot/loader.asm
+++ boot/loader.asm
@@ -4,17 +4,22 @@ org	0100h
 	jmp	start
 
 start:
+	mov	ax, cs
+	mov	ds, ax
+	mov	es, ax
+	mov	ss, ax
+
 	mov	ax, .prompt
 	mov	bp, ax
 	mov	ah, 13h
 	mov	al, 01h
 	mov	cx, .str_len
-	mov	dx, 0
+	mov	dx, 0600h
 	mov	bx, 000ch
 	int	10H
 
 .prompt:
-	db	'wagaga'
+	db	"wagaga"
 	.str_len	equ	$-.prompt
-	resb	LD_SIZE - ($ - $$)
+	resb	LD_SIZE - 2 - ($ - $$)
 	db	55h, 0aah
